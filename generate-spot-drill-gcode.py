5d30f4fe1a8f5cdd0702c7bc6ab40404f4b7623d
eburdick <ed@edburdick.net>
2021-11-17 11:06:12 -0500

    Snapshot. Testing push from laptop.

--- generate-spot-drill-gcode.py
+++ generate-spot-drill-gcode.py
@@ -14,13 +14,24 @@ from tkinter import ttk              # more widgets
 from tkinter.scrolledtext import ScrolledText  # not sure why we need to import a module for scrolled text
 from tkinter import filedialog
 
+# constants for list index values
+XVALUE = 0;
+YVALUE = 1;
+XWIDGET = 2;
+YWIDGET = 3;
+
+# temporary code to open working files. This will be replaced by a file selection box later
 coordsFileName = "test_coords.txt"
 gcodeFileName = "test_gcode.nc"
 
+# open the coordinates file. This is just a pair of numbers on each line, x, then y, separated by a single
+# space. The readLines method creates a list of lines from this file, then we split each line into the two
+# fields (split()) and remove the end of line (strip())
 coordsFile = open(coordsFileName, "r")
 coordsLines = coordsFile.readlines()
 for line in coordsLines:
     coords = line.strip().split(' ')
+    # just print the coords value for now.
     print(coords)
 #    print(line.strip())  # line.strip() removes end of line characters
 
@@ -51,20 +62,32 @@ s.configure('window.TFrame', font=('Helvetica', 20))
 window.title('Spot Drilling Tool')
 window.geometry("400x600")
 
+#
+# create a list for each coordinate pair consisting of the value of X, the value of Y, and two tk
+# entry widgets. The widgets will be displayed in the GUI window. Each of these coordinate pair lists
+# will be appended to the coordList, previously initialized as an empty list, resulting in a list of
+# coordinate pair lists.
+#
 i=0
 for line in coordsLines:
     coords = line.strip().split(' ')
-    coordList.append([coords[0], coords[1], Entry(window, coords[0]), Entry(window, coords[1])])
+    coordList.append([coords[XVALUE], coords[YVALUE], Entry(window), Entry(window)])
+    coordList[i][XWIDGET].insert(END,coordList[i][XVALUE])
+    coordList[i][YWIDGET].insert(END,coordList[i][YVALUE])
+    i = i + 1
 
 
-print(coordList)
+print(coordList)  # debug temp
+
 #
-# X and Y entry widgets
+# Place X and Y entry widgets in the window
 #
+i=0
+for lineNumber in range(4):
+    coordList[lineNumber][XWIDGET].grid(row=i,column=0)
+    coordList[lineNumber][YWIDGET].grid(row=i,column=1)
+    i = i + 1
 
-coordList[0][2].grid(row=0,column=0)
-coordList[0][3].grid(row=0,column=1)
-coordList
 #yEntry[0].grid(row=0,column=1)
 #xEntry[1].grid(row=1,column=0)
 #yEntry[1].grid(row=1,column=1)
